36ba775c980409e9239bac458b03565ab45d0edf
jahid <you@example.com>
2019-02-21 22:10:26 +0600

    Serving HTML pages

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const fs = require('fs');
 
 const server = http.createServer( (req, res) => {
     console.log("Request was made. " + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-
-    const myReadStream = fs.createReadStream(`${__dirname}/readme.txt`, 'utf-8');
-    const myWriteStream = fs.createWriteStream(__dirname+'/writeme.txt');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    const myReadStream = fs.createReadStream(`${__dirname}/index.html`, 'utf8');
+    
     myReadStream.pipe(res);
 });
 server.listen(3000, '127.0.0.1');
