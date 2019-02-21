4a00152b635baca86b3f302eb98ce7baaff0dab9
jahid <you@example.com>
2019-02-21 21:46:10 +0600

    Usign pipe to http response

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const http = require('http');
 const fs = require('fs');
 
-const myReadStream = fs.createReadStream(`${__dirname}/readme.txt`, 'utf-8');
-const myWriteStream = fs.createWriteStream(__dirname+'/writeme.txt');
 
-myReadStream.on('data' , chunk => {
-    console.log("new chunk recived");
-    myWriteStream.write(chunk);
+const server = http.createServer( (req, res) => {
+    console.log("Request was made. " + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    const myReadStream = fs.createReadStream(`${__dirname}/readme.txt`, 'utf-8');
+    const myWriteStream = fs.createWriteStream(__dirname+'/writeme.txt');
+    myReadStream.pipe(res);
 });
-
-
-
-
-
-
-// const server = http.createServer( (req, res) => {
-//     console.log("Request was made. " + req.url);
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end('Hey ninjas');
-// });
-// server.listen(3000, '127.0.0.1');
-// console.log("yo dawgs, now listining to port 3000");
+server.listen(3000, '127.0.0.1');
+console.log("yo dawgs, now listining to port 3000");
