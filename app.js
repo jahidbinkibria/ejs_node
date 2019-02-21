1d57d240c407d28af86a2dc6b1f7919fb4e0f728
jahid <you@example.com>
2019-02-21 20:24:29 +0600

    createreadstream streaming and buffer

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer( (req, res) => {
-    console.log("Request was made. " + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey ninjas');
+const myReadStream = fs.createReadStream(`${__dirname}/readme.txt`, 'utf-8');
+
+myReadStream.on('data' , chunk => {
+    console.log("new chunk recived");
+    console.log("                       ");
+    console.log(chunk);
 });
-server.listen(3000, '127.0.0.1');
-console.log("yo dawgs, now listining to port 3000");
+
+
+
+
+
+
+// const server = http.createServer( (req, res) => {
+//     console.log("Request was made. " + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hey ninjas');
+// });
+// server.listen(3000, '127.0.0.1');
+// console.log("yo dawgs, now listining to port 3000");
