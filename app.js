de87fa4d25c58017f362496e69d5283d9ec5be18
jahid <you@example.com>
2019-02-21 20:33:48 +0600

    Writeable stream

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const http = require('http');
 const fs = require('fs');
 
 const myReadStream = fs.createReadStream(`${__dirname}/readme.txt`, 'utf-8');
+const myWriteStream = fs.createWriteStream(__dirname+'/writeme.txt');
 
 myReadStream.on('data' , chunk => {
     console.log("new chunk recived");
-    console.log("                       ");
-    console.log(chunk);
+    myWriteStream.write(chunk);
 });
 
 
