c3fc2c18c01f0f55a7545f20f05740b963ef9e37
jahid <you@example.com>
2019-02-21 23:13:25 +0600

    send json data

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const fs = require('fs');
 const server = http.createServer( (req, res) => {
     console.log("Request was made. " + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    const myReadStream = fs.createReadStream(`${__dirname}/index.html`, 'utf8');
-    
-    myReadStream.pipe(res);
+   
+    const myObj = {
+        name: 'Ryu',
+        job: 'Ninja',
+        age: 20
+    };
+
+    res.end(JSON.stringify(myObj));
 });
 server.listen(3000, '127.0.0.1');
 console.log("yo dawgs, now listining to port 3000");
