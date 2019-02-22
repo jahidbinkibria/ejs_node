22d18113cb5aeb98037a0add6ea44f69b29247f7
jahid <you@example.com>
2019-02-22 22:04:43 +0600

    Basic Node Routing

--- app.js
+++ app.js
@@ -4,15 +4,20 @@ const fs = require('fs');
 
 const server = http.createServer( (req, res) => {
     console.log("Request was made. " + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-   
-    const myObj = {
-        name: 'Ryu',
-        job: 'Ninja',
-        age: 20
-    };
-
-    res.end(JSON.stringify(myObj));
+    if(req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    }else if(req.url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    }else if(req.url === '/api/ninjas'){
+        var ninjas = [{name: 'ryu', age: 29}, {name: 'youshi', age:32}];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(ninjas));
+    }else{
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
 });
 server.listen(3000, '127.0.0.1');
 console.log("yo dawgs, now listining to port 3000");
