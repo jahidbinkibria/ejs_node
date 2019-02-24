148d10be74ad3ef2645f941340fef5fe4d2ad29d
jahid <you@example.com>
2019-02-24 18:33:47 +0600

    call file by sendfile func

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 app.get('/' , (req,res) => {
-    res.send("This is the home page");
+    res.sendFile( `${__dirname}/index.html`);
 });
 
 app.get('/contact' , (req,res) => {
-    res.send("This is the contact page");
+    res.sendFile(`${__dirname}/contact.html`);
 });
 
 app.get('/profile/:id', (req, res) =>{
